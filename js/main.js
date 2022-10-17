3050b77639f6667978c21971224aa070f6d17064
EyeMLeo <Leonid.Akuta@gmail.com>
2022-10-17 22:17:17 +0200

    HTML and Css mainly done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
 'use strict'
 console.log('main.js');
+
+
+/*
+Player Opponent
+
+paper rock          player wins
+paper scissors      Oppoinent wins
+paper paper         draw  
+
+rock paper          Opponent wins
+rock scissors       player wins
+rock rock           draw
+
+scissors paper      player wins
+scissors rock       opponent wins
+csissors scissors   draw
+
+
+
+
+
+*/
+
+
+let opponentNumber=(Math.random()*2+1).toFixed(0)
+console.log(opponentNumber)
+
+Let opponentChoice;
+// if (opponentNumber===1) {
+    
+// }
